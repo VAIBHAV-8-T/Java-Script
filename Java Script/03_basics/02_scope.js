c234031bc45af0e843dfa0be241994bef108b894
Harshit Vaibhav <harshitvaibhav1610@gmail.com>
2024-10-20 21:23:01 +0530

    iteration

--- Java Script/03_basics/02_scope.js
+++ Java Script/03_basics/02_scope.js
@@ -1,12 +1,36 @@
  let a =300
   if (true)   {
     let a = 10
-    const b =20
-    console.log("INNER:",a);
+    // const b =20
+    // console.log("INNER:",a);
 }
   
     
 
 
-console.log(a);
-console.log(b);
+// console.log(a);
+// console.log(b);
+
+function one(){
+    const username = "vaibhav"
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+    // console.log(website)
+    two()
+}
+// one{}
+
+
+
+// ====== interesting======
+addone(5)
+function addone(num){
+    return num + 1
+}
+addTwo(5)
+
+const addTwo = function(num){
+    return num +2
+}
---
+++ Java Script/05_iterations/four.js
@@ -0,0 +1,14 @@
+const myObject = {
+    js:'javqscript',
+    cpp: 'c++',
+    rb:"ruby",
+    swift:"swift by apple"
+}
+for(const key in myObject){
+console.log(`${key} shortcut isfor ${myObject[key]}`)
+}
+
+const programming = ["js", "rb","py","java"]
+for (const key in programming) {
+ console.log(programming[key])   ;
+}
